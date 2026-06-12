adb92846d5f310174708ca0c75d2b80364c6f44b
Sanzida Sultana <sanzidasinthy080303@gmail.com>
2026-06-12 13:38:38 +0600

    Create 9. Access list items.py

---
+++ 9. Access list itams.py
@@ -0,0 +1,22 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+#This will return the items from position 2 to 5.
+
+#Remember that the first item is position 0,
+#and note that the item in position 5 is NOT included
+
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+
+#This will return the items from index 0 to index 4.
+
+#Remember that index 0 is the first item, and index 4 is the fifth item
+#Remember that the item in index 4 is NOT included
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+
+#This will return the items from index 2 to the end.
+
